472e16eb9d900dc612fe4469898411a658fb9601
Peter Åslund <peter@peteraslund.me>
2019-09-06 13:10:03 +0200

    PlayingCard models

---
+++ src/ProbabilityAnalyser.Core/Models/PlayingCardSuit.cs
@@ -0,0 +1,14 @@
+﻿namespace ProbabilityAnalyser.Core.Models
+{
+	public enum PlayingCardSuit
+	{
+		None = 0,
+
+		Clubs,
+		Diamonds,
+		Hearts,
+		Spades,
+
+		Joker,
+	}
+}
---
+++ src/ProbabilityAnalyser.Core/Models/SolitareCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProbabilityAnalyser.Core.Models
+{
+	public class PlayingCard
+	{
+		public PlayingCard(PlayingCardSuit suit, PlayingCardRank rank)
+		{
+			Suit = suit;
+			Rank = rank;
+		}
+
+		public PlayingCardSuit Suit { get; }
+		public PlayingCardRank Rank { get; }
+	}
+}
