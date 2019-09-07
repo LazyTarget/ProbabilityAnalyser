205b92d9cbbc669fd96223268b103584482a4d95
Peter Åslund <peter@peteraslund.me>
2019-09-07 20:14:18 +0200

    Added tests

---
+++ src/ProbabilityAnalyser.UnitTests/AcesUpTests.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProbabilityAnalyser.Core.Models;
+using ProbabilityAnalyser.Core.Program;
+
+namespace ProbabilityAnalyser.UnitTests
+{
+	[TestClass]
+	public class AcesUpTests
+	{
+		protected virtual int RunInstance()
+		{
+			var deck = PlayingCardDeck.Standard52CardDeck();
+
+			var program = new AcesUp();
+			var points = program.Run(deck);
+
+			Console.WriteLine("AcesUp :: Result = {0}", points);
+			return points;
+		}
+
+
+		[TestMethod]
+		public void TestInstances()
+		{
+			int NR_OF_INSTANCES = 10;
+
+			Parallel.For(0, NR_OF_INSTANCES, (i, s) =>
+			{
+				RunInstance();
+			});
+		}
+
+	}
+}
