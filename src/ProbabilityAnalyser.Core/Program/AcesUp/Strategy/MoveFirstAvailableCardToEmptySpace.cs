a7d2e3b27641effcb3c3b6ddc105c9f72ebe34d8
Peter Åslund <peter@peteraslund.me>
2019-09-10 10:30:29 +0200

    Static prioritizer for "MoveFirstAvailableCardToEmptySpace"

--- src/ProbabilityAnalyser.Core/Program/AcesUp/Strategy/MoveFirstAvailableCardToEmptySpace.cs
+++ src/ProbabilityAnalyser.Core/Program/AcesUp/Strategy/MoveFirstAvailableCardToEmptySpace.cs
@@ -12,6 +12,14 @@ namespace ProbabilityAnalyser.Core.Program.AcesUp.Strategy
 		{
 		}
 
+		protected override IEnumerable<AcesUpPile> Prioritize(AcesUpRunContext context)
+		{
+			yield return context.FaceUpCards.Pile1;
+			yield return context.FaceUpCards.Pile2;
+			yield return context.FaceUpCards.Pile3;
+			yield return context.FaceUpCards.Pile4;
+		}
+
 		protected override bool Peek(AcesUpPile pile)
 		{
 			PlayingCard peek;
