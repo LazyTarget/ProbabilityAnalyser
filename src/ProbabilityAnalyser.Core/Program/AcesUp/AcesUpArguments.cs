06bc64a7717bf22e283fb30ef1fdc81200aace1e
Peter Åslund <peter@peteraslund.me>
2019-09-10 15:55:26 +0200

    DetermineBestStrategy

---
+++ src/ProbabilityAnalyser.Core/Program/AcesUp/AcesUpArguments.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ProbabilityAnalyser.Core.Program.AcesUp
+{
+	public class AcesUpArguments
+	{
+		public bool HardMode { get; set; }
+		public ICardMovingStrategy MovingStrategy;
+		public IPilePrioritizer Prioritizer;
+
+
+		public void ApplyTo(AcesUpArguments arguments)
+		{
+			arguments.HardMode = HardMode;
+			arguments.MovingStrategy = MovingStrategy;
+			arguments.Prioritizer = Prioritizer;
+		}
+
+		public void ApplyTo(AcesUpRunContext context)
+		{
+			context.HardMode = HardMode;
+			context.MovingStrategy = MovingStrategy;
+			context.Prioritizer = Prioritizer;
+		}
+	}
+}
