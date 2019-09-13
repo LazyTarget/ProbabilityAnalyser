911c501d5dd4ca6f7d90fdefa805ec27ff8a5049
Peter Åslund <peter@peteraslund.me>
2019-09-13 09:46:57 +0200

    IAcesUp

---
+++ src/ProbabilityAnalyser.Core/Program/AcesUp/IAcesUp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProbabilityAnalyser.Core.Program.AcesUp
+{
+	public interface IAcesUp
+	{
+		int Run(AcesUpRunContext context);
+	}
+}
