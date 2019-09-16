fc0cd50a5f3e9af5b784005827442c1a49236411
Peter Åslund <peter@peteraslund.me>
2019-09-16 15:26:20 +0200

    IPlayingCardBuilder

---
+++ src/ProbabilityAnalyser.Core/Intefaces/IPlayingCardBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProbabilityAnalyser.Core.Models;
+
+namespace ProbabilityAnalyser.Core.Intefaces
+{
+	public interface IPlayingCardBuilder
+	{
+		ISuitedPlayingCardBuilder Clubs();
+		ISuitedPlayingCardBuilder Spades();
+		ISuitedPlayingCardBuilder Diamonds();
+		ISuitedPlayingCardBuilder Heart();
+		PlayingCard Joker();
+	}
+
+	public interface ISuitedPlayingCardBuilder
+	{
+		PlayingCard Rank(PlayingCardRank rank);
+	}
+}
